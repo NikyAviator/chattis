36c402d1001cb60756be5d673bf69e081be6aa68
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-01-09 12:39:17 +0100

    Skeleton for MessageForm and Sidebar completed

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
-
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import '../../scss/main.scss';
 const MessageForm = () => {
-  return <div>MessageForm</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <div className='messages-output'></div>
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col md={11}>
+            <Form.Group>
+              <Form.Control
+                type='text'
+                placeholder='Your message'
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col md={1}>
+            <Button variant='primary' type='submit'>
+              <p>Skicka</p>
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default MessageForm;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const rooms = ['first room', 'second room', 'third room'];
+
+  return (
+    <>
+      <h3>Available rooms:</h3>
+      <ListGroup>
+        {rooms.map((room, index) => (
+          <ListGroup.Item action variant='light' key={index}>
+            {room}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <h3>Members:</h3>
+    </>
+  );
 };
 
 export default Sidebar;
