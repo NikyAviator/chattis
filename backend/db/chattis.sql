a3d0637d61fb376b2bb004ccfc513f7d7cd83aaa
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-01-09 11:01:03 +0100

    Reworked The db.sql

--- backend/db/chattis.sql
+++ backend/db/chattis.sql
@@ -22,31 +22,32 @@ CREATE TABLE "chat_users"(
     "user_id" uuid NOT NULL,
     "blocked" BOOLEAN NOT NULL,
     "invitation_accepted" BOOLEAN NOT NULL,
-    "creator" BOOLEAN NOT NULL
+    "creator" BOOLEAN NOT NULL,
+    CONSTRAINT "chat_users_chat_id_foreign" FOREIGN KEY("chat_id") REFERENCES "chats"("id"),
+    CONSTRAINT "chat_users_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id")
 );
 
 CREATE TABLE "messages"(
     "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     "chat_id" uuid NOT NULL,
-    "timestamp" DATE NOT NULL
+    "from_id" uuid NOT NULL,
+    "message_timestamp" TIMESTAMP NOT NULL,
+    "content" VARCHAR(1000) NOT NULL,
+    CONSTRAINT "messages_chat_id_foreign" FOREIGN KEY("chat_id") REFERENCES "chats"("id"),
+    CONSTRAINT "from_id_foreign" FOREIGN KEY (from_id) REFERENCES "users"("id")
 );
 
 CREATE TABLE "user_blockings"(
     "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     "user_id" uuid NOT NULL,
-    "blocked_user_id" uuid NOT NULL
+    "blocked_user_id" uuid NOT NULL,
+  CONSTRAINT "user_blockings_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id"),
+  CONSTRAINT "user_blockings_blocked_user_id_foreign" FOREIGN KEY("blocked_user_id") REFERENCES "users"("id")
 );
 
-ALTER TABLE
-    "chat_users" ADD CONSTRAINT "chat_users_chat_id_foreign" FOREIGN KEY("chat_id") REFERENCES "chats"("id");
-ALTER TABLE
-    "chat_users" ADD CONSTRAINT "chat_users_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
-ALTER TABLE
-    "messages" ADD CONSTRAINT "messages_chat_id_foreign" FOREIGN KEY("chat_id") REFERENCES "chats"("id");
-ALTER TABLE
-    "user_blockings" ADD CONSTRAINT "user_blockings_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
-ALTER TABLE
-    "user_blockings" ADD CONSTRAINT "user_blockings_blocked_user_id_foreign" FOREIGN KEY("blocked_user_id") REFERENCES "users"("id");
+
+
+
 
 CREATE TABLE "session" (
   "sid" varchar NOT NULL COLLATE "default",
