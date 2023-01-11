b36ff1d5e4302046fad969ac70241fd269272593
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-01-11 17:55:19 +0100

    Added some spaces in routes.cjs for easier reading

--- backend/server/api/routes.cjs
+++ backend/server/api/routes.cjs
@@ -4,32 +4,45 @@ const controller = require('./controllers.cjs');
 
 router.get('/sse', controller.sse);
 
-// REGISTER/CREATE USER - DONE
+// REGISTER/CREATE USER - DONE - Y
 router.post('/user/register', controller.createUser);
-// LOGIN USER - DONE
+
+// LOGIN USER - DONE - Y
 router.post('/user/login', controller.loginUser);
-// LOG OUT USER - DONE
+
+// LOG OUT USER - DONE - Y
 router.delete('/user/logout', controller.logoutUser);
-// FETCH ONE USER - DONE
+
+// FETCH ONE USER - DONE - Y (getLoggedInUser)
 router.get('/fetch-user', controller.fetchUser);
-// BLOCK A USER - DONE
+
+// BLOCK A USER - DONE - Y (men minus :id)
 router.post('/user/block/:id', controller.blockUser);
+
 // GET ALL TITLES AND ID FROM ALL THE CHATS - DONE
 router.get('/chats', controller.getChats);
+
 // GET CHAT USERS THAT ARE USING A CHAT (group in a chat) - DONE
 router.get('/chat/users', controller.getChatUsers);
+
 // CREATE A CHAT - DONE
 router.post('/chat/create', controller.createChat);
+
 // INVITE TO CHAT - DONE
 router.post('/chat/invite/:id', controller.inviteToChat);
+
 // ACCEPT INVITE TO CHAT - TODO
 router.post('/chat/accept-invite/:id', controller.acceptChatInvite);
+
 // BAN FROM CHAT - TODO
 router.post('/chat/ban/:id', controller.banFromChat);
+
 // SEND A MESSAGE - MAYBE DONE?
 router.post('/chat/message', controller.sendMessage);
+
 // GET A CHAT MESSAGE - TODO
 router.get('/chat/messages/:id', controller.getChatMessages);
+
 // DELETE A MESSAGE - TODO
 router.delete('/chat/delete-message/:id', controller.deleteMessage);
 
